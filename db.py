bf90e14f79b1572dbf46e77b7856634e373f2bf4
Yoav Steinberg <yoyo.steinberg@gmail.com>
2022-07-28 00:49:57 +0300

    Changed table structure and added remove to db

--- db.py
+++ db.py
@@ -2,6 +2,7 @@
 
 import sqlite3
 import atexit
+
 con = sqlite3.connect('places.db')
 
 cur = con.cursor()
@@ -13,28 +14,31 @@ cur.execute('''CREATE TABLE IF NOT EXISTS places
                 name TEXT, link TEXT, rating INTEGER)''')
 cur.execute('''CREATE TABLE IF NOT EXISTS criterions
                (criterion_id INTEGER PRIMARY KEY,
-                name TEXT, description TEXT)''')
+                name TEXT)''')
 cur.execute('''CREATE TABLE IF NOT EXISTS place_criterions
-               (place_id INTEGER, criterion_id INTEGER,
+               (place_id INTEGER, criterion_id INTEGER, description TEXT,
                 value TEXT)''')
 # con.commit()
 
 # Insert a row of data
 # cur.execute("INSERT INTO places VALUES (0, 'Fabiano', 'google.com', 4)")
-# cur.execute('''INSERT INTO criterions VALUES (0, 'Vegeterian', 'Percentage of non-meat dishes')''')
-# cur.execute('''INSERT INTO criterions VALUES (1, 'Gluten-free', 'Percentage of gluten-free dishes')''')
-# cur.execute('''INSERT INTO place_criterions VALUES (0, 1, "80%")''')
+# cur.execute('''INSERT INTO criterions VALUES (0, 'Vegeterian')''')
+# cur.execute('''INSERT INTO criterions VALUES (1, 'Gluten-free')''')
+# cur.execute('''INSERT INTO place_criterions VALUES (0, 1, 'Percentage of gluten-free dishes', "80%")''')
 
 # Save (commit) the changes
 con.commit()
 max_places_id = next(cur.execute("SELECT MAX(place_id) FROM places"))[0]
-max_criterions_id = next(cur.execute("SELECT MAX(criterion_id) FROM criterions"))[0]
+max_criterion_id = next(cur.execute("SELECT MAX(criterion_id) FROM criterions"))[0]
+
 
 # We can also close the connection if we are done with it.
 # Just be sure any changes have been committed or they will be lost.
 
 def search_by_name(name):
     return cur.execute("SELECT * FROM places WHERE name=?", (name,))
+
+
 def get_place_profile(id):
     place = cur.execute("SELECT * FROM places WHERE place_id=?", (id,))
     try:
@@ -42,10 +46,11 @@ def get_place_profile(id):
     except StopIteration:
         raise ValueError("Place %d does not exist" % id)
     criterions = cur.execute('''
-SELECT c.name, c.description, pc.value FROM criterions c
+SELECT c.name, pc.description, pc.value FROM criterions c
 JOIN place_criterions pc ON pc.criterion_id = c.criterion_id
     ''')
     return place[1], place[2], place[3], list(criterions)
+
 
 def add_place(name, link, rating):
     global max_places_id
@@ -54,12 +59,24 @@ def add_place(name, link, rating):
                                                            name, link, rating))
     con.commit()
 
+
+def remove_place(id):
+    cur.execute("DELETE FROM places WHERE place_id=?", (id,))
+    con.commit()
+
+
 def add_criterion(name, description):
-    global max_criterions_id
-    max_criterions_id += 1
-    cur.execute("INSERT INTO criterions VALUES (?, ?, ?)", (max_criterions_id,
+    global max_criterion_id
+    max_criterion_id += 1
+    cur.execute("INSERT INTO criterions VALUES (?, ?, ?)", (max_criterion_id,
                                                             name, description))
     con.commit()
+
+
+def remove_criterion(id):
+    cur.execute("DELETE FROM criterions WHERE place_id=?", (id,))
+    con.commit()
+
 
 def add_place_criterion(place_id, criterion_id, value):
     cur.execute("INSERT INTO place_criterions VALUES (?, ?, ?)", (place_id,
--- web_phonebook.py
+++ web_phonebook.py
@@ -1,14 +1,17 @@
 from bottle import route, run, template, request, redirect, response, static_file
 import db
-import re
 import os
+
+
 @route('/add_place')
 def add_restaurant_page():
     pass
 
+
 @route('/add_criterion')
 def add_criteion_page():
     pass
+
 
 @route('/')
 def main_page():
@@ -17,19 +20,21 @@ def main_page():
     template_file = os.path.join(template_dir, template_name)
     try:
         name = str(request.query.name)
-        phone = str(request.query.phone)
-        finds = [db.get_place_profile(0)]
+        finds = [db.get_place_profile(0), db.get_place_profile(1), db.get_place_profile(2)]
+        print(finds)
         print('requested name:', name)
     except ValueError as e:
         print('no name request')
         finds = []
     return template(template_file, finds=finds)
 
+
 @route('/add_place')
 def add_place():
     name, link, rating = request.query.name, request.query.link, request.query.rating
     db.add_place(name, link, rating)
     redirect("/")
+
 
 if __name__ == '__main__':
     run()
